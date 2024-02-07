6fd31427b7d91f7431520e614172d9186caca00c
IMMGabriel <mateiciuc.gabriel00@gmail.com>
2024-02-07 12:10:49 +0200

    finished script//can be played multiple times

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ function addPlayerScore(){
     playerScore++;
     document.querySelector('.PlayerCount').innerText = `Player: ${playerScore}`;
     console.log(`PlayerScore: ${playerScore}`);
-};
+    if(playerScore === 5){
+      document.querySelector(".FinishScreen").style.display = 'flex';
+      }
+    }
 function addBotScore(){
     botScore++
     document.querySelector('.BotCount').innerHTML = `Computer: ${botScore}`;
     console.log(`BotScore: ${botScore}`);
-};
+    if(botScore === 5){
+      document.querySelector(".FinishScreen").style.display = 'flex';
+      }
+    }
 
-function oneGame(playerPick,botPick){
 
-}
 
 
 function botPick(){
@@ -31,8 +35,62 @@ function botPick(){
         return "SCISSORS"
     }
 
+console.log(botPick)
 }
 
 
+// function myPick(button){
+//   var picked = button.className;
+//   console.log(` ${picked}`)
+//   return picked;
+// }
+
+
+function oneRound(x){
+  playerPick = x
+  console.log(`Player pick = ${playerPick}`)
+  botbotPick = botPick()
+  console.log(`Bot pick = ${botbotPick}`)
+  result = (`${playerPick}`+ " - " + `${botbotPick}`)
+  console.log(result)
+  if (playerPick === botbotPick) {
+    console.log("tie");
+    
+  } else {
+    switch (playerPick) {
+      case "ROCK":
+        if (botbotPick === "PAPER") {
+          addBotScore();
+        } else if (botbotPick === "SCISSORS") {
+          addPlayerScore();
+        }
+        break;
+      case "PAPER":
+        if (botbotPick === "SCISSORS") {
+          addBotScore();
+        } else if (botbotPick === "ROCK") {
+          addPlayerScore();
+        }
+        break;
+        
+
+      case "SCISSORS":
+        if (botbotPick === "ROCK") {
+          addBotScore();
+        } else if (botbotPick === "PAPER") {
+          addPlayerScore();
+        }
+        break;
+    }
+  }
+}
+
+function newGame(){
+  document.querySelector(".FinishScreen").style.display = 'none';
+  document.querySelector('.PlayerCount').innerText = `Player: 0`;
+  document.querySelector('.BotCount').innerHTML = `Computer: 0`;
+  botScore = 0
+  playerScore = 0
+}
 
 
